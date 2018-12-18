e62684eec1089023c0e2c63fa7f4e283a23960fe
minli <minli@vmware.com>
2018-12-18 09:44:34 +0800

    Remove Duplicates from Sorted Array

---
+++ Array/Easy/Best_Time_to_Buy_and_Sell_StockII.py
@@ -0,0 +1,13 @@
+import math
+
+def maxProfit(nums):
+    maxfpt = 0
+    for x in range(1, len(nums)):
+        if nums[x] > nums[x-1]:
+            maxfpt += nums[x] - nums[x-1]
+    return maxfpt
+
+
+if __name__ == '__main__':
+    a = [7, 1, 5, 3, 6, 4]
+    print maxProfit(a)
