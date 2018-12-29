9b61e009a1a72fcb7cb18cdd2a4b89eab42c15cf
minli <minli@vmware.com>
2018-12-29 16:23:50 +0800

    Remove Duplicates from Sorted Array

--- Array/Easy/Single_Number.py
+++ Array/Easy/Single_Number.py
@@ -1,7 +1,14 @@
-def singleNumber1(self, nums):
+def singleNumber_dict(nums):
     dic = {}
     for num in nums:
         dic[num] = dic.get(num, 0)+1
     for key, val in dic.items():
         if val == 1:
             return key
+
+def SingleNumber_modulo(nums):
+    return reduce(lambda x, y: x ^ y, nums)
+
+
+if __name__ == '__main__':
+    print SingleNumber_modulo([1, 2, 1, 4, 4, 5, 5])
