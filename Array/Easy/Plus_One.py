946984caf0bc8e7aade9fabbf87d719c18ef0698
minli <minli@vmware.com>
2019-01-22 17:51:45 +0800

    Remove Duplicates from Sorted Array

--- Array/Easy/Plus_One.py
+++ Array/Easy/Plus_One.py
@@ -5,7 +5,7 @@ def plus_one(nums):
 def plus_one_recursion(nums):
     n = len(nums)
     if nums[-1] != 9:
-         return nums[0:n-1] + [nums[-1] +1]
+        return nums[0:n-1] + [nums[-1] +1]
     elif n > 1:
         return plus_one_recursion(nums[0:n-1]) + [0]
     else:
